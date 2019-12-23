e0c7d535bc02199c8a6f7d93fd9a72d44847b190
Srishti Kashyap <srishtikashyap1@gmailcom>
2019-12-23 16:39:55 +0530

    Srishti : GUI

--- gui.py
+++ gui.py
@@ -1,1 +1,7 @@
-print("hello")
+import tkinter as tk
+from tkinter import ttk
+win = tk.Tk()
+win.title('WSN Optimization')
+nodes_label = ttk.Label(win, text='No. of nodes : ')
+nodes_label.grid(row=0, column=0)
+win.mainloop()
